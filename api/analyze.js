43398ff5df2662aa392b1eb1f6466bb6ad4feb3b
trynarunit <prawnapha2545@gmail.com>
2026-07-13 12:48:23 +0700

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  const API_KEY = "";
   const { text } = req.body;
   const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
     method: 'POST',
